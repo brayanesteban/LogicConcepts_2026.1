1bfd061c1b53304a982982a158bbacf2b2c973b5
BRAYAN ESTEBAN MONTOYA LONDO�O <brayanmontoya1126335@correo.itm.edu.co>
2026-02-06 19:10:21 -0500

    Complete excercice 01

---
+++ LogicConcepts/IsNumberOddOrNot/Program.cs
@@ -0,0 +1,28 @@
+﻿var numberString = string.Empty;
+do
+{
+    Console.Write("ingrese Numero entero o la palabra 'salir' para salir ");
+    numberString = Console.ReadLine();
+    if (numberString!.ToLower() == "Salir")  
+    {
+        continue;
+    }
+
+    var numberInt = 0;
+    if (int.TryParse(numberString, out numberInt))
+
+        if (numberInt % 2 == 0)
+        {
+            Console.WriteLine($"El numero {numberInt} es par");
+
+        }
+        else
+        {
+            Console.WriteLine($"El numero {numberInt} es impar");
+        }
+    else
+        {
+            Console.WriteLine($"El valor ingresado {numberString}, no es un numero entero");
+        }
+} while (numberString.ToLower() != "salir");
+Console.WriteLine("game over");
