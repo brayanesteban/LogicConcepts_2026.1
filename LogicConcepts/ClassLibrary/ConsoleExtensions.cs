92e8aaab5e7a9b6fdb33cfab6bca8d9457c08f0d
BRAYAN ESTEBAN MONTOYA LONDO�O <brayanmontoya1126335@correo.itm.edu.co>
2026-02-06 20:13:45 -0500

    Complete 02 and Complete the carpet shared

---
+++ LogicConcepts/ClassLibrary/ConsoleExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace Shared
+{
+    public class ConsoleExtensions
+    {
+        public static int GetInt(string message)
+        {
+            Console.Write(message);
+            var input = Console.ReadLine();
+            if (int.TryParse(input, out int result))
+            {
+                return result;
+            }
+            throw new Exception("el valor ingresado no es un numero valido");
+        }
+
+    }
+}
---
+++ LogicConcepts/TheGreathestNumber/Program.cs
@@ -0,0 +1,38 @@
+﻿
+using Shared;
+
+var response = string.Empty;
+do
+
+{
+    try
+    {
+        Console.WriteLine("Ingrese 3 numeros diferentes: ");
+
+        var number1 = ConsoleExtensions.GetInt("ingrese primer numero: ");
+        var number2 = ConsoleExtensions.GetInt("ingrese segundo numero: ");
+        var number3 = ConsoleExtensions.GetInt("ingrese tercer numero: ");
+
+        if (number1 > number2 && number1 > number3)
+        {
+            Console.WriteLine($"El numero mayor es: {number1}");
+        }
+        else if (number2 > number1 && number2 > number3)
+        {
+            Console.WriteLine($"El numero mayor es: {number2}");
+        }
+        else
+        {
+            Console.WriteLine($"El numero mayor es: {number3}");
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+
+
+
+    Console.Write("desea continuar S/N ");
+    response = Console.ReadLine()!.ToUpper();
+} while (response == "S");
