033d576cee09853fa85139bd151f46aa4997c609
BRAYAN ESTEBAN MONTOYA LONDO�O <brayanmontoya1126335@correo.itm.edu.co>
2026-02-08 14:08:36 -0500

    Complete exercice 04

---
+++ LogicConcepts/IsMultipleOrAnother/Program.cs
@@ -0,0 +1,22 @@
+﻿using Shared;
+using System.ComponentModel.Design;
+
+Console.WriteLine("ingresa dos numeros por favor");
+
+do
+{
+
+    var a = ConsoleExtensions.GetInt("ingrese primer numero: ");
+    var b = ConsoleExtensions.GetInt("ingrese segundo numero: ");
+
+    if (b % a == 0)
+    {
+        Console.WriteLine($"el numero {a}, es múltiplo de {b}");
+    }
+    else 
+    {
+        Console.WriteLine($"el numero {a} , no es multiplo de  {b}");
+    }
+
+} while (true);
+
