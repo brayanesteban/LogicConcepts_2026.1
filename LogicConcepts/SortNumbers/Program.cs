08052b56459a14a44dde6bdb4c1eb577a6f3cab0
BRAYAN ESTEBAN MONTOYA LONDO�O <brayanmontoya1126335@correo.itm.edu.co>
2026-02-08 13:22:04 -0500

    SortNumbers Complete

---
+++ LogicConcepts/SortNumbers/Program.cs
@@ -0,0 +1,55 @@
+﻿using Shared;
+
+do
+{
+    Console.WriteLine("Ingrese tres numeros para ordenarlos de mayor a menor");
+
+    var number1 = ConsoleExtensions.GetInt("ingrese primer numero: ");
+    var number2 = ConsoleExtensions.GetInt("ingrese segundo numero: ");
+    if (number1 == number2)
+    {
+        Console.WriteLine("Los numeros no pueden ser iguales, por favor ingrese numeros diferentes");
+        continue;
+    }
+    var number3 = ConsoleExtensions.GetInt("ingrese tercer numero: ");
+    if (number2==number3 || number3 == number1)
+    {
+        Console.WriteLine("Los numeros no pueden ser iguales, por favor ingrese numeros diferentes");
+        continue;
+    }
+
+    if (number1 > number2 && number1 > number3)
+    {
+        if (number2 > number3)
+        {
+            Console.WriteLine($"El mayor es: {number1} el medio es {number2} y el menor es {number3}");
+        }
+        else
+        {
+            Console.WriteLine($"El mayor es: {number1} el medio es {number3} y el menor es {number2}");
+        }
+
+    }
+    else if (number2 > number1 && number2 > number3)
+    {
+            if (number1 > number3)
+            {
+                Console.WriteLine($"El mayor es: {number2} el medio es {number1} y el menor es {number3}");
+            }
+            else
+            {
+                Console.WriteLine($"El mayor es: {number2} el medio es {number3} y el menor es {number1}");
+            }
+        }
+        else
+        {
+            if (number1 > number2)
+            {
+                Console.WriteLine($"El mayor es: {number3} el medio es {number1} y el menor es {number2}");
+            }
+            else
+            {
+                Console.WriteLine($"El mayor es: {number3} el medio es {number2} y el menor es {number1}");
+            }
+        }
+    } while (true); 
