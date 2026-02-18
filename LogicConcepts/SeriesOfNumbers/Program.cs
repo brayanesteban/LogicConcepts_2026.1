3245817e8fdb2678e0155e688c523f2c51ce9f85
BRAYAN ESTEBAN MONTOYA LONDO�O <brayanmontoya1126335@correo.itm.edu.co>
2026-02-17 23:24:47 -0500

    exercice 12 not complete for now

---
+++ LogicConcepts/SeriesOfNumbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using Shared;
+
+var answer = String.Empty;
+var options = new List<string> { "si", "no" };
+
+do
+{
+  var n = ConsoleExtensions.GetInt("Cuantos numeros desea: ");
+    int sum = 0;
+
+    for (int i = 1; i  <= n; i++)
+    {
+        Console.WriteLine($"{i}\t");
+        sum += i;   
+    }
+    Console.WriteLine();
+    Console.WriteLine($"La suma de los numeros es: {sum,20:N0}");
+    Console.WriteLine($"El promedio de los numeros es: {sum / n,20:N2}");
+
+
+    do
+    {
+        answer = ConsoleExtensions.GetValidOptions("desea iniciar otra vez? (si/no): ", options);
+    } while (!options.Any(x => string.Equals(x, answer, StringComparison.CurrentCultureIgnoreCase)));
+
+} while (answer!.Equals("si", StringComparison.CurrentCultureIgnoreCase));
+
+Console.WriteLine("Game Over");
